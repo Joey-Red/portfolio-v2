b8d57a1cc7e0635637cdf893bb479ec782de6b98
Joey <xjoey96@gmail.com>
2022-09-20 15:57:07 -0500

    update portfolio

--- src/Components/MemoryGame.js
+++ src/Components/MemoryGame.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
-import memoryGame from "../img/PjScreenshots2/MemoryGame.png";
+import FEP1 from "../img/PjScreenshots2/FEP1.png";
+import FEP2 from "../img/PjScreenshots2/FEP2.png";
+import FEP3 from "../img/PjScreenshots2/FEP3.png";
+import FEP4 from "../img/PjScreenshots2/FEP4.png";
+
 function MemoryGame() {
   let [activePhoto, setActivePhoto] = useState(1);
 
@@ -31,9 +35,34 @@ function MemoryGame() {
             className="faColor"
           ></FontAwesomeIcon>
         </button>
-        <div className="projImageContainer">
-          <img src={memoryGame} alt="memory game" />
-        </div>
+        {activePhoto === 1 ? (
+          <div className="projImageContainer">
+            <img src={FEP1} alt="Free Event Planner 1" />
+          </div>
+        ) : (
+          <></>
+        )}
+        {activePhoto === 2 ? (
+          <div className="projImageContainer">
+            <img src={FEP2} alt="Free Event Planner 2" />
+          </div>
+        ) : (
+          <></>
+        )}
+        {activePhoto === 3 ? (
+          <div className="projImageContainer">
+            <img src={FEP3} alt="Free Event Planner 3" />
+          </div>
+        ) : (
+          <></>
+        )}
+        {activePhoto === 4 ? (
+          <div className="projImageContainer">
+            <img src={FEP4} alt="Free Event Planner 4" />
+          </div>
+        ) : (
+          <></>
+        )}
         <button
           className="screenshotButton screenshotRight none"
           onClick={nextPhoto}
@@ -46,26 +75,27 @@ function MemoryGame() {
       </div>
 
       <div className="descrContainer">
-        <h3>Memory Game</h3>
+        <h3>Free Event Planner</h3>
         <div className="projectDescription">
-          This is an older project, and it's quite simple. It is a Memory Game.
-          You just have to click each of the pictures, but if you click the same
-          one twice before clicking all of them your score will be reset. This
-          project does not have a back end, and uses React. Not sure what I was
-          thinking with that gradient background.
+          This project is a blog using the MERN stack. Where you can create
+          events, where you will be provided a link to send to other people. It
+          also lists public events for anyone to join! It uses a monorepo where
+          the React front end is hosted on Github Pages, the Node/Express
+          backend are hosted on heroku. This project uses MongoDB for the
+          database.
         </div>
         <div className="projectLinks">
           <a
             rel="noreferrer"
             target="_blank"
-            href="https://joey-red.github.io/memory-game/"
+            href="https://joey-red.github.io/EventPlanner/"
           >
             Live project link
           </a>
           <a
             rel="noreferrer"
             target="_blank"
-            href="https://github.com/Joey-Red/memory-game"
+            href="https://github.com/Joey-Red/EventPlanner"
           >
             Link to the Github repo
           </a>
