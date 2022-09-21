b1a2a5d9204920cd1a36d9601a725906bdd11def
Joey <xjoey96@gmail.com>
2022-09-21 04:00:07 -0500

    fixed carousel, got rid of keyboard support, will fix later

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -17,53 +17,81 @@ import MusicWithFriends from "./MusicWithFriends";
 function Projects() {
   let [activeProject, setActiveProject] = useState(0);
 
-  document.addEventListener("keydown", logKey);
-  function logKey(e) {
-    document.removeEventListener("keydown", logKey);
-    if (e.code !== "ArrowRight" && e.code !== "ArrowLeft") {
-      return;
-    } else if (`${e.code}` === "ArrowRight") {
-      nextProject();
-    } else if (`${e.code}` === "ArrowLeft") {
-      prevProject();
-    }
-  }
+  // document.addEventListener("keydown", logKey);
+  // function logKey(e) {
+  //   document.addEventListener("keydown", logKey);
 
+  //   document.removeEventListener("keydown", logKey);
+  //   if (e.code !== "ArrowRight" && e.code !== "ArrowLeft") {
+  //     return;
+  //   } else if (`${e.code}` === "ArrowRight") {
+  //     nextProject(activeProject);
+  //   } else if (`${e.code}` === "ArrowLeft") {
+  //     prevProject(activeProject);
+  //   }
+  // }
+
+  // function projectSwitch(activeProject) {
+  //   switch (activeProject) {
+  //     case 0:
+  //       return <BlogApi />;
+  //     case 1:
+  //       return <MemoryGame />;
+  //     case 2:
+  //       return <WheresPikachu />;
+  //     case 3:
+  //       return <RedditProject />;
+  //     case 4:
+  //       return <MembersOnly />;
+  //     case 5:
+  //       return <MusicWithFriends />;
+  //     // default:
+  //     //   return <BlogApi />;
+  //   }
+  // }
   function projectSwitch(activeProject) {
-    switch (activeProject) {
-      case 0:
-        return <MusicWithFriends />;
-      case 1:
-        return <BlogApi />;
-      case 2:
-        return <WheresPikachu />;
-      case 3:
-        return <RedditProject />;
-      case 4:
-        return <MembersOnly />;
-      case 5:
-        return <MemoryGame />;
-      default:
-        return <BlogApi />;
+    if (activeProject === 0) {
+      return <BlogApi />;
+    }
+    if (activeProject === 1) {
+      return <MemoryGame />;
+    }
+    if (activeProject === 2) {
+      return <WheresPikachu />;
+    }
+    if (activeProject === 3) {
+      return <RedditProject />;
+    }
+    if (activeProject === 4) {
+      return <MembersOnly />;
+    }
+    if (activeProject === 5) {
+      return <MusicWithFriends />;
+    } else {
+      let randomProj = Math.floor(Math.random() * 6) + 1;
+      setActiveProject(randomProj);
+      // return <BlogApi />;
     }
   }
 
-  let nextProject = useCallback(() => {
-    if (activeProject === 5) {
+  let newCount = activeProject;
+  let nextProject = () => {
+    newCount = activeProject + 1;
+    if (newCount === 6) {
       setActiveProject(0);
     } else {
-      setActiveProject(activeProject + 1);
+      setActiveProject(newCount);
     }
-    // }
-  }, [activeProject]);
-
-  let prevProject = useCallback(() => {
-    if (activeProject === 1) {
+  };
+  let prevProject = () => {
+    newCount = activeProject - 1;
+    if (newCount === -1) {
       setActiveProject(6);
+      newCount = 6;
     } else {
-      setActiveProject(activeProject - 1);
+      setActiveProject(newCount);
     }
-  }, [activeProject]);
+  };
 
   return (
     <div className="projectPageContainer">
@@ -73,11 +101,11 @@ function Projects() {
           All of these projects are currently live. Under the description is a
           link, if you'd like to get a better look.
         </p>
-        <p className="mobileBar">
+        {/* <p className="mobileBar">
           <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>You can use your
           arrow keys to scroll through my projects!
           <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
-        </p>
+        </p> */}
       </div>
       <div className="projectsContainer">
         <div id="activeProject">
